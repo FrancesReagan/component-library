249f96622dca1f6d69c926f7fc3e71c1241d0d8e
Frances Reagan James <adtgwen@gmail.com>
2025-06-10 11:24:21 -0400

    first commmit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+
+// import './App.css'//
+import AlertBox from "./components/AlertBox/AlertBox";
+
+function App() {
+  const handleClose = (alertType: string) => {
+    console.log(`Closing ${alertType} alert`);
+
+  };
+
+  return (
+    <>
+      <h1>Components Library</h1>
+      <AlertBox 
+      type="success" 
+      message="Hello World"
+      onClose={() => handleClose("success")}/>
+      <AlertBox 
+      type="error" 
+      message="Hello World"
+      onClose={() => handleClose("error")}
+      />
+      <AlertBox 
+      type="info" 
+      message="Hello World"
+      onClose={() =>handleClose("info")}
+      />
+      <AlertBox 
+      type="warning" 
+      message="Hello World"
+      onClose={() => handleClose("warning")}/>
+    </>
+  )
+}
+
+export default App
